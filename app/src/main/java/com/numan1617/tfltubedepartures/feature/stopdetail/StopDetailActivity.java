230de6d0089106c9e121aab899cae3bd5cf8e3a3
James Newman <numan1617@gmail.com>
2016-04-18 23:53:30 +0100

    Add stop detail activity

---
+++ app/src/main/java/com/numan1617/tfltubedepartures/feature/stopdetail/StopDetailActivity.java
@@ -0,0 +1,13 @@
+package com.numan1617.tfltubedepartures.feature.stopdetail;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import com.numan1617.tfltubedepartures.R;
+
+public class StopDetailActivity extends AppCompatActivity {
+  @Override protected void onCreate(@Nullable final Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_stop_detail);
+  }
+}
